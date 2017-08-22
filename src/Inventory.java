45a7bfcd5358fd2da555707cea774641a75f2496
juan <juanarrechea90@gmail.com>
2017-08-21 23:03:28 -0700

    Finished Item class Doc pending
    Worked on Inventory, should finish doc and sorting next

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,77 @@
+import java.util.HashMap;
+
 public class Inventory {
+	public static final String ITEM_ID = "ID";
+	public static final String ITEM_NAME = "Item Name";
+	
+	private HashMap<String, Item> inventory;
+	private Item[] sortedInventory;   //Its sorted only when called so items are sorted as they get added/Get added
+	
+	
+	/***** Constructors *****/
+	
+	/**
+	 * 
+	 */
+	public Inventory() {
+		inventory = new HashMap<String, Item>();
+	}
+	
+	/**
+	 * @param inventory
+	 */
+	public Inventory(HashMap<String, Item> inventory) {
+		this.inventory = inventory;
+	}
+	/**
+	 * @param name
+	 * @param item
+	 */
+	public Inventory(String name, Item item) {
+		inventory = new HashMap<String, Item>();
+		inventory.put(name, item);
+	}
+	/**
+	 * @param item
+	 */
+	public Inventory(Item item) {
+		inventory = new HashMap<String, Item>();
+		inventory.put(item.getValueForKey(ITEM_NAME), item);
+	}
+	
+	/***** Setters *****/
+	/**
+	 * @param itemName
+	 * @param attribute
+	 * @param value
+	 */
+	public void setAttribute(String itemName, String attribute, String value) {
+		Item item = null;
+		
+		if (itemName != null) {
+			item = inventory.get(itemName);
+		}
+		if (item !=null) {
+			item.setAttribute(attribute, value);
+		}
+	}
+	
+	
+	/***** Getters *****/
+	
+	
+	/***** Other *****/
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return inventory.toString();		
+	}
+	
+	/***** Helpers *****/
+	
+	
     
 }
--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,9 @@
 import java.util.HashMap;
 
+/**
+ * @author juan
+ *
+ */
 public class Item {
     private HashMap<String, String> attributes;
     
@@ -7,15 +11,25 @@ public class Item {
     
     /**** Constructors *****/
 
+    /**
+     * 
+     */
     public Item(){
         this.attributes = new HashMap<String, String>();
     }
 
+    /**
+     * @param attributes
+     */
     public Item(HashMap<String, String> attributes){
         if (attributes != null){
             this.attributes = new HashMap<String, String>(attributes);
         }
     }
+    /**
+     * @param attributes
+     * @param values
+     */
     public Item(String[] attributes, String[] values){
         if (attributes != null && values != null){
             this.attributes = new HashMap<String, String>();
@@ -24,33 +38,56 @@ public class Item {
             }
         }
     }
+    /**
+     * @param attribute
+     * @param value
+     */
     public Item (String attribute, String value) {
-    		if (attribute != null && value != null){
-    			this.attributes = new HashMap<String, String>();
-    			this.attributes.put(attribute, value);
-    		}
+		if (attribute != null && value != null){
+			this.attributes = new HashMap<String, String>();
+			this.attributes.put(attribute, value);
+		}
     }
     
     
     /**** Setters *****/
+    /**
+     * @param attribute
+     * @param newValue
+     */
     public void setAttribute(String attribute, String newValue) {
     		attributes.put(attribute, newValue);
     }
     
     
     /**** Getters *****/
+    /**
+     * @param attribute
+     * @return
+     */
     public String getValueForKey(String attribute) {
     		return attributes.get(attribute);
     }
     
     /**** Other *****/
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     @Override
     public String toString() {
     		return attributes.toString();
     }
+    /**
+     * @param attribute
+     * @return
+     */
     public boolean containsKey(String attribute) {
     		return attributes.containsKey(attribute);
     }
+    /**
+     * @param value
+     * @return
+     */
     public boolean containsValue(String value) {
     		return attributes.containsValue(value);
     }
